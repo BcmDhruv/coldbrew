6d4baa6046c9bd67b85cf4ec2852e3b47294e494
dhruvrijwani <146342395+dhruvrijwani@users.noreply.github.com>
2025-07-31 18:27:28 +0530

    Your commit message here

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -12,56 +12,63 @@ const Team = () => {
                 <div className="row">
                     <div className="team-member-wrapper">
                         <div className="team-circle">
-                        <img src="https://blackcoffee.media/wp-content/themes/betheme/img/team/Kirtan.png" alt="Kirtan" />
+                        <img src="/team/Kirtan.png" alt="Kirtan" />
                         </div>
                         <h3>Kirtan Mankad</h3>
                         <p>Co - Founder</p>
                     </div>
                     <div className="team-member-wrapper">
                         <div className="team-circle">
-                        <img src="https://blackcoffee.media/wp-content/themes/betheme/img/team/2.png" alt="Siddharth" />
+                        <img src="/team/sid.png" alt="Siddharth" />
                         </div>
                         <h3>Siddharth Padmanabhan</h3>
                         <p>Co - Founder</p>
                     </div>
                     <div className="team-member-wrapper">
                         <div className="team-circle">
-                        <img src="https://blackcoffee.media/wp-content/uploads/2025/06/Nikhil-sir.png" alt="Siddharth" />
+                        <img src="/team/Sairi.png" alt="Siddharth" />
                         </div>
-                        <h3>Siddharth Padmanabhan</h3>
-                        <p>Lorem Ipsum</p>
+                        <h3>Sairindhri Sengupta</h3>
+                        <p>Creative Director</p>
                     </div>
                 </div>
                 <div className="row">
                     <div className="team-member-wrapper">
                         <div className="team-circle">
-                        <img src="https://blackcoffee.media/wp-content/uploads/2025/06/Nikhil-sir.png" alt="Siddharth" />
+                        <img src="/team/Neel.png" alt="Siddharth" />
                         </div>
-                        <h3>Siddharth Padmanabhan</h3>
-                        <p>Lorem Ipsum</p>
+                        <h3>Neelakantan Krishnan</h3>
+                        <p>Client Success Manager</p>
                     </div>
                     <div className="team-member-wrapper">
                         <div className="team-circle">
-                        <img src="https://blackcoffee.media/wp-content/uploads/2025/06/Nikhil-sir.png" alt="Siddharth" />
+                        <img src="/team/Jalnidhi.png" alt="Siddharth" />
                         </div>
-                        <h3>Siddharth Padmanabhan</h3>
-                        <p>Lorem Ipsum</p>
+                        <h3>Jalnidhi Saurabh Singh</h3>
+                        <p>Junior Copywriter</p>
+                    </div>
+                    <div className="team-member-wrapper">
+                        <div className="team-circle">
+                        <img src="/team/Ankit.png" alt="Siddharth" />
+                        </div>
+                        <h3>Ankit Solanki</h3>
+                        <p>Senior Graphic Designer</p>
                     </div>
                 </div>
                 <div className="row">
                     <div className="team-member-wrapper">
                         <div className="team-circle">
-                        <img src="https://blackcoffee.media/wp-content/uploads/2025/06/sahil-sir.png" alt="Siddharth" />
+                        <img src="/team/Parneet.png" alt="Siddharth" />
                         </div>
-                        <h3>Siddharth Padmanabhan</h3>
-                        <p>Lorem Ipsum</p>
+                        <h3>Parneet Kaur</h3>
+                        <p>Experience Designer</p>
                     </div>
                     <div className="team-member-wrapper">
                         <div className="team-circle">
-                        <img src="https://blackcoffee.media/wp-content/uploads/2025/06/sahil-sir.png" alt="Siddharth" />
+                        <img src="/team/Bilal.png" alt="Siddharth" />
                         </div>
-                        <h3>Siddharth Padmanabhan</h3>
-                        <p>Lorem Ipsum</p>
+                        <h3>Mohammad Bilal</h3>
+                        <p>Junior Graphic Designer</p>
                     </div>
                 </div>
             </div>
