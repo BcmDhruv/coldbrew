4cf7a6b2ade25bc2e086d95f3032686f61ddebee
dhruvrijwani <146342395+dhruvrijwani@users.noreply.github.com>
2025-08-01 18:26:34 +0530

    Initial commit from Webflow export

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -11,13 +11,13 @@ const AboutUs = () => {
                 <h1>About Us</h1>
             </div>
             <div className="aboutus-main">
-                <p>We read the room. Then build for it.</p>
+                <p><strong>We read the room. Then build for it.</strong> </p>
                 <p>ColdBrew is a creative studio obsessed with one thing: <br />knowing what your audience atually cares about.</p>
                 <p>We dig into the scrolls, swipes, searches, and silences—so we can craft work that doesn’t just look good, but lands exactly right.</p>
                 <p>From performance ads that convert to content systems that stay relevant, from brand films to websites that feel right at first click—everything we make is rooted in the real world, not guesswork.</p>
                 <p>We’ve built for fast-moving eCommerce brands, culture-led lifestyle labels, mobility apps, and purpose-first campaigns. And in every space, one thing stays the same: <br />We know how to catch the pulse—and create for it.</p>
                 <p>Smart, strategic, scroll-stopping creative. Backed by insight. Powered by taste. Designed to move people and performance.</p>
-                <p>This is ColdBrew. We get your audience—sometimes before they do.</p>
+                <p><strong>This is ColdBrew.<br />We get your audience—sometimes before they do.</strong></p>
             </div>
         </div>
     </section>
