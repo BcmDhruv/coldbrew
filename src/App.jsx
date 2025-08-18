432761baed748e415add5b66d0f0c8edcde4b2dc
dhruvrijwani <146342395+dhruvrijwani@users.noreply.github.com>
2025-08-18 17:13:47 +0530

    Pushing edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
